b30eaa1b390dba325a44adbc09904333a973d133
hyogij <hyogij@gmail.com>
2016-04-27 09:31:54 +0900

    Polish some codes

--- BleakNumber/main.c
+++ BleakNumber/main.c
@@ -36,12 +36,17 @@ void isBleakNumber(int n) {
 	++msb;
 
 	for (int i = msb; i >= 0; i--) {
-		// i is the number of set bits
-		// If it satisfies above condition then you are done
+		// i is the number of set bits. If it satisfies above condition then you are done
 		if (setbits(n - i) == i) {
 			printf("Supported Number! %d and supported by : %d\n", n, n - i);
 			return;
 		}
 	}
 	printf("Bleak Number! %d\n", n);
+}
+
+// Assume following return types while writing the code for this question.
+char* output1[100];
+void bleakNumbers(int input1[]) {
+	//Write code here
 }
